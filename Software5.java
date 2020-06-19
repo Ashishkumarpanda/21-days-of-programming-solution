1cb33aa219cd4bcf0c9038f6a86f4ee0ba9828b1
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2020-06-19 15:12:10 +0530

    Add files via upload

---
+++ Software5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Software5 {
+    public static void main(String args[])
+    {
+      String s;
+      char i;
+      
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the binary value");
+      s=sc.nextLine();
+      int d=Integer.parseInt(s,2);
+      i=(char)d;
+      System.out.println("the character will be:"+i);
+      
+
+
+
+     
+
+    }
+    
+}
