bec8bc37bed8e1c51e5a201876cf9a074e792281
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2020-06-28 18:45:34 +0530

    Add files via upload

---
+++ Software14.java
@@ -0,0 +1,19 @@
+import  java.lang.String;
+import java.util.Scanner;
+public class Software14{
+    public static void main(String[] args) {
+        String c,a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("The original string is");
+        c=sc.nextLine();
+        System.out.println("The substring you want to replace");
+        a=sc.nextLine();
+        System.out.println("The string you want to use instead ");
+        b=sc.nextLine();
+        String d=c.replaceAll(a,b);
+        System.out.println(d);
+        
+        
+
+    }
+}
